0b20e7f8df43f426a6f0178ae0a9a65c771c0820
Denis Stepulenok <super.denis@gmail.com>
2012-08-05 20:27:42 +0400

    Основные рабочие классы

---
+++ ELangCUrlManager.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Расширение для обработки URL
+ */
+class ELangCUrlManager extends CUrlManager
+{
+    public function createUrl($route, $params = array(), $ampersand = '&')
+    {
+        if (!isset($params['lang']))
+            $params['lang'] = Yii::app()->GetLanguage();
+        return parent::createUrl($route, $params, $ampersand);
+    }
+}
+
---
+++ ELangHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Компонент для переключения сайта на язык "по-умолчанию"
+ */
+class ELangHandler extends CApplicationComponent
+{
+    public $languages = array();
+    public $strict = false;
+
+    public function init()
+    {
+        array_push($this->languages, Yii::app()->getLanguage());
+        $this->parseLanguage();
+    }
+
+    private function parseLanguage()
+    {
+        Yii::app()->urlManager->parseUrl(Yii::app()->getRequest());
+        if (!isset($_GET['lang'])) {
+            $defaultLang = Yii::app()->getRequest()->getPreferredLanguage();
+            if (!$this->strict) {
+                $tmp = explode('_', $defaultLang);
+                $defaultLang = $tmp[0];
+            }
+            if (in_array($defaultLang, $this->languages))
+                Yii::app()->setLanguage($defaultLang);
+            else
+                Yii::app()->setLanguage($this->languages[0]);
+        } elseif ($_GET['lang'] != Yii::app()->getLanguage() && in_array($_GET['lang'], $this->languages)) {
+            Yii::app()->setLanguage($_GET['lang']);
+        }
+    }
+}
